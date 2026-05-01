631a45f6ee433d0be354aeb317eda8dabd53766f
Noah Sherwin <noahrsherwin@gmail.com>
2026-05-01 14:41:44 -0700

    chore(deps-dev, vscode): add yarn dev-deps, workspace configs

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+import baseConfig from '@halospv3/hce.shared-config/commitlintConfig';
+
+const scopes = {
+  CHANGELOG: 'Affects "CHANGELOG.md".',
+  commitlint:
+    'Affects "src/commitlintConfig.ts", its tests, the repo\'s commitlint config, or anything else related to commitlint.',
+  deps: 'Affects dependencies required at runtime.',
+  'deps-dev':
+    'Affects dependencies required in the dev environment or during build time.',
+  eslint:
+    'Affects "src/eslintConfig.ts", its tests, or the repo\'s ESLint config.',
+  package: 'Affects "package.json" or its tests.',
+  README: 'Affects "README.md".',
+  release: 'Reserved for release commits.',
+  renovate: 'Affects "./.github/renovate.json".',
+  TODO: 'Affects "TODO.md".',
+  vscode: 'Affects ".vscode/".',
+  VSLSMNA: 'Affects "src/Vintage Story | Left-Side Mod Notification Actions.user.js".',
+  VSSMTWARA: 'Affects "src/Vintage Story | Strip Mod Title Whitespace And Replace Ampersand.user.js".'
+};
+
+/**@type {import('@commitlint/types').UserConfig} */
+const config = {
+  ...baseConfig,
+  rules: {
+    ...baseConfig.rules,
+    'scope-enum': [
+      2,
+      'always',
+      Object.keys(scopes),
+    ],
+  },
+};
+export default config;
---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import baseConfig from '@halospv3/hce.shared-config/eslintConfig';
+import { defineConfig } from 'eslint/config';
+
+const config = defineConfig(...baseConfig);
+export default config;
