3bf967718bc1cfe66e7a36f19b0d23db1b044f16
Noah Sherwin <noahrsherwin@gmail.com>
2026-05-01 22:17:02 -0700

    fix(DGDGSU): get multiple GameSDK URLs; await async

--- src/Discord | Get Discord GameSDK URL.user.js
+++ src/Discord | Get Discord GameSDK URL.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/BinToss/UserScripts
 // @homepageURL https://github.com/BinToss/UserScripts
 // @updateUrl   https://github.com/BinToss/UserScripts/raw/refs/heads/main/src/Discord%20%7C%20Get%20Discord%20GameSDK%20URL.user.js
-// @version     1.0.3
+// @version     1.0.4
 // @author      BinToss
 // @icon        https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6266bc493fb42d4e27bb8393_847541504914fd33810e70a0ea73177e.ico
 // @match       https://discord.com/developers/docs/game-sdk/sdk-starter-guide
@@ -11,19 +11,27 @@
 // @grant       none
 // @description 7/15/2021, 7:05:57 PM
 // ==/UserScript==
-async function getGameSDKURL()
-{
-  while (document.querySelector("a[href^='#get-set-up']") === null)
-    {
-      await new Promise(resolve => setTimeout(resolve, 200));
-    }
-  var discordGameSDK_URL = document.querySelector("a[href^='https://dl-game-sdk.discordapp.net']").href;
-  console.log('Discord GameSDK URL is...')
-  console.log(discordGameSDK_URL);
+async function getGameSDKURL() {
+  while (document.querySelector('a[href^="#get-set-up"]') === null)
+    await new Promise(resolve => setTimeout(resolve, 200));
+  const links = [
+    ...document.querySelectorAll('a[href^="https://dl-game-sdk.discordapp.net"]'),
+  ].map(elm =>
+    'href' in elm && typeof elm.href === 'string'
+      ? elm.href
+      : null,
+  ).filter(v => v !== null);
+  console.log('Discord GameSDK URLs are...');
+  for (const link of links)
+    console.log(link);
 }
 
-if (document.readyState === 'loading') {  // Loading hasn't finished yet
-document.addEventListener('COMContentLoaded', getGameSDKURL);
-} else {  // `DOMContentLoaded` has already fired
-  getGameSDKURL();
+if (document.readyState === 'loading') { // Loading hasn't finished yet
+  document.addEventListener(
+    'DOMContentLoaded',
+    () => { void getGameSDKURL(); },
+  );
+}
+else { // `DOMContentLoaded` has already fired
+  await getGameSDKURL();
 }
