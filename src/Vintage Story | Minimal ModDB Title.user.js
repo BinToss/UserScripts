e11fbebf6d6c2a0f9673d4ebdbdc2e4a768a8179
Noah Sherwin <noahrsherwin@gmail.com>
2026-05-01 21:19:28 -0700

    style(VSMMT): indent homepageURL;  unwrap body

--- src/Vintage Story | Minimal ModDB Title.user.js
+++ src/Vintage Story | Minimal ModDB Title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Vintage Story | Minimal ModDB Title
 // @namespace     https://github.com/BinToss/UserScripts
-// @homepageURL https://github.com/BinToss/UserScripts
+// @homepageURL   https://github.com/BinToss/UserScripts
 // @updateUrl     https://github.com/BinToss/UserScripts/raw/refs/heads/main/src/Vintage%20Story%20%7C%20Minimal%20ModDB%20Title.user.js
 // @version       1.0.0
 // @author        BinToss
@@ -13,5 +13,4 @@
 // @description   8/29/2025, 1:36:28 PM
 //  Strips ' - Vintage Story Mod DB' from the title of a mod's page. Intended for short-named browser bookmarks.
 // ==/UserScript==
-window.
-document.title = document.title.replace(' - Vintage Story Mod DB', '');
+window.document.title = document.title.replace(' - Vintage Story Mod DB', '');
